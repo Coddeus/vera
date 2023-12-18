2052e5d62a247bbb6be24a5e8b313bbbb5f28361
Coddeus <codeusbonus@gmail.com>
2023-12-18 11:28:41 +0100

    📜 Example transformation combination Vertex, model, view, projection transformations together

--- examples/dev/lib/src/lib.rs
+++ examples/dev/lib/src/lib.rs
@@ -10,28 +10,25 @@ pub fn get() -> Input {
     unsafe {
         D_RANDOM_VERTEX_COLOR = true;
         D_VERTEX_COLOR = [1.0, 1.0, 1.0];
-        D_VERTEX_ALPHA = 0.8;
+        D_VERTEX_ALPHA = 1.0;
     }
 
-    let t1 = Triangle::new(
-        Vertex::new().pos(0.0, 0.5, 0.0),
-        Vertex::new().pos(0.3, 0.2, 0.0),
-        Vertex::new().pos(-0.2, 0.4, 0.0),
+    let t1 = Model::from_vertices(
+        vec![
+            Vertex::new().pos(0.5, 0.5, 0.0).rgb(1.0, 1.0, 1.0),
+            Vertex::new().pos(-0.5, 0.5, 0.0).rgb(0.5, 0.5, 0.5),
+            Vertex::new().pos(0.5, -0.5, 0.0).rgb(0.5, 0.5, 0.5),
+            Vertex::new().pos(0.5, -0.5, 0.0).rgb(0.5, 0.5, 0.5),
+            Vertex::new().pos(-0.5, 0.5, 0.0).rgb(0.5, 0.5, 0.5),
+            Vertex::new().pos(-0.5, -0.5, 0.0).rgb(0.0, 0.0, 0.0),
+        ]
     );
 
     Input {
         m: vec![
-            Model::from_vertices(vec![
-                Vertex::new().pos(0.0, 0.0, 0.0),
-                Vertex::new().pos(0.5, 0.5, 0.5)
-                .transform(Transformation::Translate(-0.5, -0.5, 0.0)).start(0.0).end(1.0)
-                .transform(Transformation::Translate(0.5, 0.5, 0.0)).start(1.0).end(2.0)
-                .transform(Transformation::RotateZ(PI * 2.0)).start(0.0).end(2.0),
-                Vertex::new().pos(0.0, 1.0, 0.0),
-            ]),
-            t1.transform(Transformation::Scale(2.0, 2.0, 2.0))
+            t1.transform(Transformation::Scale(2.0, 2.0, 2.0)).start(0.0).end(2.0)
         ],
-        v: View::new(),
-        p: Projection::new(),
+        v: View::new().transform(Transformation::Lookat(2.0, -2.0, -5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)).start(0.0).end(0.0),
+        p: Projection::new().transform(Transformation::Perspective(-1.0, 1.0, -1.0, 1.0, 1.0, 10.0)).start(0.0).end(0.0),
     }
 }
